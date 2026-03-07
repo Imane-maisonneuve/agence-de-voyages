72ded2ff19f114c5728aaa87430dc85ae763b6dc
Imane <i.elmostaqim.ca@gmail.com>
2026-03-06 20:08:50 -0500

    correction coquille

--- src/components/ManyForfaits.jsx
+++ src/components/ManyForfaits.jsx
@@ -3,14 +3,24 @@ import SingleForfait from "./SingleForfait";
 import { Link, useLocation } from "react-router-dom";
 
 const ManyForfaits = ({ forfaits, onDelete }) => {
-  return (
-    <>
+  let divAddForfait = "";
+  if (location.pathname === "/forfaits") {
+    divAddForfait = (
       <div className="flex justify-end">
         <Link to="/AddForfait" className="text-blue-700 hover:underline">
           Ajouter un forfait
         </Link>
       </div>
-
+    );
+  }
+  return (
+    <>
+      {divAddForfait}
+      {/* <div className="flex justify-end">
+        <Link to="/AddForfait" className="text-blue-700 hover:underline">
+          Ajouter un forfait
+        </Link>
+      </div> */}
       <div className="grid grid-cols-3 gap-4">
         {forfaits.map((forfait) => (
           <SingleForfait
